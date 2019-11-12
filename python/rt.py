afb95ec06ef16438449376a7e7027a804f2bbee8
sethcoder <defectiveseth@gmail.com>
2019-11-12 18:51:49 -0500

    update

--- python/rt.py
+++ python/rt.py
@@ -2,7 +2,7 @@ import random
 import time
 import serial
 
-serial_device="COM5"
+serial_device="/dev/ttyUSB0"
 serial_baud="57600"
 ser = serial.Serial(serial_device,serial_baud,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,xonxoff=0,timeout=.01,rtscts=0)
 while True:
