48cc6e1a51d92db6cd41c4c9bc0b5267503bf4b9
sethcoder <defectiveseth@gmail.com>
2019-11-15 14:51:38 -0500

    bridge work

--- serial_bridge/bridge.py
+++ serial_bridge/bridge.py
@@ -141,29 +141,28 @@ print("CityXen Serial Bridge now active")
 # Main program, check for encoding method then loop
 if(encoding=="16B"):
     while True:
-        x=ser.readline().lstrip()
+        x=ser.readline().lstrip('\x00').rstrip("\n\r")
 
         if(len(x)):
-            print("["+x.rstrip("\n\r")+"]")
-            print("IN STRLEN:"+str(len(x))+":"+x.rstrip("\n\r"))
+            print("IN STRLEN:"+str(len(x))+":"+x)
 
         if(len(x)>16):
-            gp[12]=False if x[1] =="1" else True
-            gp[7] =False if x[2] =="1" else True
-            gp[11]=False if x[3] =="1" else True
-            gp[13]=False if x[4] =="1" else True
-            gp[15]=False if x[5] =="1" else True
-            gp[19]=False if x[6] =="1" else True
-            gp[21]=False if x[7] =="1" else True
-            gp[23]=False if x[8] =="1" else True
-            gp[16]=False if x[9] =="1" else True
-            gp[18]=False if x[10]=="1" else True
-            gp[22]=False if x[11]=="1" else True
-            gp[40]=False if x[12]=="1" else True
-            gp[38]=False if x[13]=="1" else True
-            gp[36]=False if x[14]=="1" else True
-            gp[32]=False if x[15]=="1" else True
-            gp[37]=False if x[16]=="1" else True
+            gp[12]=False if x[0] =="1" else True
+            gp[7] =False if x[1] =="1" else True
+            gp[11]=False if x[2] =="1" else True
+            gp[13]=False if x[3] =="1" else True
+            gp[15]=False if x[4] =="1" else True
+            gp[19]=False if x[5] =="1" else True
+            gp[21]=False if x[6] =="1" else True
+            gp[23]=False if x[7] =="1" else True
+            gp[16]=False if x[8] =="1" else True
+            gp[18]=False if x[9] =="1" else True
+            gp[22]=False if x[10]=="1" else True
+            gp[40]=False if x[11]=="1" else True
+            gp[38]=False if x[12]=="1" else True
+            gp[36]=False if x[13]=="1" else True
+            gp[32]=False if x[14]=="1" else True
+            gp[37]=False if x[15]=="1" else True
             set_gpio()
 
         counter=counter+1
